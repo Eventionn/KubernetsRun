669b4e8d4cbce9e0bebc7d360945bce81592c1ef
L-Anjo <luisanjo17@gmail.com>
2025-06-03 22:37:44 +0100

    fix: ensure Redis is initialized before dependent pods start

--- start.sh
+++ start.sh
@@ -13,9 +13,19 @@ echo "🧩 Aplicando os serviços e APIs..."
 kubectl apply -f services/
 
 echo ""
+echo "🔐 Criando Secret TLS para o NGINX..."
+kubectl create secret tls nginx-tls-secret \
+  --cert=nginx/tls.crt \
+  --key=nginx/tls.key \
+  --dry-run=client -o yaml | kubectl apply -f -
+
+echo ""
 echo "🌐 Aplicando a configuração do NGINX..."
 kubectl apply -f nginx/
 
 echo ""
 echo "✅ Finalizado!"
 kubectl get pods -A
+
+echo "🚀 Expondo Portas..."
+minikube tunnel
